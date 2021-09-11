231b37e2ca020425ea1899e7ad5ef72537c16202
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-09-11 22:34:48 +0530

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 const videoElem = document.getElementById("videoElem");
 const resultElem = document.getElementById("results");
 const LoginElem = document.getElementById("Login");
+const logbox = document.getElementById("logbox");
 const QueryImageCapture = document.getElementById("QueryImageCapture");
+const startProctoringbtn = document.getElementById("startProctoring");
 const inputImgEl = document.createElement("img");
 const queryImgEl = document.createElement("img");
+const liveImgEl = document.createElement("img");
 
 //for login
 var inputLabel;
@@ -13,20 +16,33 @@ var ouputScore;
 const height = 480;
 const width = 640;
 
+faceCheckInterval = 1000;
+
 navigator.mediaDevices.getUserMedia({
   video: true
 }).then(async (stream) => {
+
   videoElem.srcObject = stream;
+  //auth
   await run();
-  //main chain
+
 }).catch((err) => {
   console.log(err);
 });
 
+//event logger
 addLog = (data) => {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(data));
   resultElem.appendChild(li);
+  logbox.scrollTop = logbox.scrollHeight;
+}
+
+async function proct() {
+  addLog("-x- Procting started");
+  addLog("-x- Live webcam picture captured");
+  //start monitoring
+  faceCheck = setInterval(async () => { await CheckFace(); }, faceCheckInterval);
 }
 
 //authentication
@@ -34,7 +50,7 @@ async function run() {
   await faceapi.loadFaceLandmarkModel('https://propview.ap-south-1.linodeobjects.com/')
   await faceapi.loadFaceRecognitionModel('https://propview.ap-south-1.linodeobjects.com/')
   await faceapi.nets.ssdMobilenetv1.loadFromUri('https://propview.ap-south-1.linodeobjects.com/')
-  addLog("Face Recognistion model loaded");
+  addLog("-x- Face Recognistion model loaded");
   LoginElem.style = "display: box";
   LoginElem.addEventListener('click', async function (ev) {
     // await updateReferenceImageResults("https://img.lovepik.com/photo/50152/4070.jpg_wh860.jpg")
@@ -42,7 +58,8 @@ async function run() {
     ev.preventDefault();
   }, false);
   QueryImageCapture.addEventListener('click', async function (ev) {
-    takepicture();
+    takepicture(queryImgEl);
+    addLog("-x- Webcam picture captured");
     await updateQueryImageResults();
     ev.preventDefault();
   }, false);
@@ -66,17 +83,16 @@ async function updateReferenceImageResults(url) {
   const resizedResults = faceapi.resizeResults(fullFaceDescriptions, inputImgEl)
   const labels = faceMatcher.labeledDescriptors
     .map(ld => ld.label)
-  addLog(`${labels.length} face Detected in refrence image`);
+  addLog(`-x- ${labels.length} face Detected in refrence image`);
   if (labels.length === 1) {
     resizedResults.forEach(({ detection, descriptor }) => {
       const label = faceMatcher.findBestMatch(descriptor).toString();
       inputLabel = label;
       inputScore = detection.score;
-      console.log(inputLabel);
-      console.log(inputScore);
     })
     LoginElem.style = "display: none";
     QueryImageCapture.style = "display: box";
+    addLog("-x- Refrence Image Processed");
   } else {
     addLog(`check refernce image`);
     LoginElem.style = "display: none";
@@ -85,16 +101,15 @@ async function updateReferenceImageResults(url) {
 }
 
 //capture query image
-function takepicture() {
+function takepicture(imageElement) {
   const tempCanvas = document.createElement("canvas");
   var context = tempCanvas.getContext('2d');
   tempCanvas.width = width;
   tempCanvas.height = height;
   context.drawImage(videoElem, 0, 0, width, height);
   var data = tempCanvas.toDataURL('image/png');
-  queryImgEl.setAttribute('src', data);
-  queryImgEl.style = "-webkit-transform: scaleX(-1); transform: scaleX(-1);"
-  addLog("Webcam picture captureed");
+  imageElement.setAttribute('src', data);
+  imageElement.style = "-webkit-transform: scaleX(-1); transform: scaleX(-1);"
   return
 }
 
@@ -116,13 +131,53 @@ async function updateQueryImageResults() {
     const label = faceMatcher.findBestMatch(descriptor).toString()
     ouputLabel = label;
     ouputScore = detection.score;
-    console.log(ouputLabel, ouputScore);
     var length = ouputLabel.length;
     var length2 = inputLabel.length;
     if (inputLabel.substring(0, length2 - 3) === ouputLabel.substring(0, length - 6)) {
       addLog(`Face sucessfully matched with ${(ouputScore * 100).toFixed(2)}% accuracy`);
+      QueryImageCapture.style = "display: none";
+      startProctoringbtn.style = "display: box";
+      startProctoringbtn.addEventListener('click', async function (ev) {
+        //start monitoring
+        await proct();
+        ev.preventDefault();
+      }, false);
     } else {
       addLog("Face is not matching");
     }
+  })
+}
+
+async function CheckFace() {
+  takepicture(liveImgEl);
+  if (!faceMatcher) {
+    return
+  }
+
+  const results = await faceapi
+    .detectAllFaces(liveImgEl, getFaceDetectorOptions())
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+
+  if (!results.length) {
+    addLog("Candidate absent");
+  }
+
+  if (results.length > 1) {
+    addLog("More than 1 person detected");
+  }
+
+  results.forEach(({ detection, descriptor }) => {
+    const label = faceMatcher.findBestMatch(descriptor).toString()
+    ouputLabel = label;
+    ouputScore = detection.score;
+    var length = ouputLabel.length;
+    var length2 = inputLabel.length;
+    if (inputLabel.substring(0, length2 - 3) === ouputLabel.substring(0, length - 6)) {
+      addLog("Candidate present");
+    } else {
+      addLog("unknown candidate");
+    }
+
   })
 }
